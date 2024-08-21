1a99da17cd3dd74c945503b9622e5d3c3e08e8ad
codemodel6 <codemodel6@gmail.com>
2024-08-21 16:56:50 +0900

    design : 헤더 틀 잡기

--- app/components/organism/TheHeader.tsx
+++ app/components/organism/TheHeader.tsx
@@ -1,15 +1,56 @@
-import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {globalDisplay} from '../../assets/styles/global/globalDisplay';
 
 const TheHeader = () => {
-  return <View style={styles.headerWrapper}></View>;
+  return (
+    <View style={styles.headerWrapper}>
+      <TouchableOpacity style={styles.headerBackButton}>
+        <Text style={styles.headerBackButtonText}>뒤</Text>
+      </TouchableOpacity>
+      <Text style={styles.headerTitle}>안녕하세요</Text>
+      <TouchableOpacity style={styles.headerSearchButton}>
+        <Text style={styles.headerSearchButtonText}>돋</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   headerWrapper: {
-    ...globalDisplay.centerRow,
-    height: 80,
+    ...globalDisplay.betweenRow,
+    height: 70,
     backgroundColor: '#000000',
+    paddingHorizontal: 10,
+    paddingTop: 10, // 상태바 고려 설정
+  },
+  headerBackButton: {
+    ...globalDisplay.centerColumn,
+    backgroundColor: '#ffffff',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  headerBackButtonText: {
+    ...globalDisplay.centerRow,
+    fontSize: 15,
+    color: '#000000',
+  },
+  headerTitle: {
+    fontSize: 20,
+    color: '#ffffff',
+  },
+  headerSearchButton: {
+    ...globalDisplay.centerColumn,
+    backgroundColor: '#ffffff',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  headerSearchButtonText: {
+    ...globalDisplay.centerRow,
+    fontSize: 15,
+    color: '#000000',
   },
 });
 
