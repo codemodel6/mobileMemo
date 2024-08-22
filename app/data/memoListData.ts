5016757768b0136edd68278b8e46417703c58da6
codemodel6 <codemodel6@gmail.com>
2024-08-22 16:11:31 +0900

    feat : 메모 데이터 map을 위한 keyNumber 추가

--- app/data/memoListData.ts
+++ app/data/memoListData.ts
@@ -9,49 +9,57 @@ export interface memoListProps {
   createdAt: string;
   // memo의 업데이트 날짜
   updatedAt: string;
+  // keyId
+  keyNumber: number;
 }
 
 export const memoListData: memoListProps[] = [
   {
-    id: 'AMIFE-6AEM-EFIAM-MEIFE',
+    id: 'AMIFE-6AEM-EFIAM-MEIFE5',
     title: 'memo의 제목입니다',
     description: 'memo의 상세 내용입니다.',
     createdAt: '2024-06-06',
     updatedAt: '2024-07-01',
+    keyNumber: 5,
   },
   {
-    id: 'AMIFE-6AEM-EFIAM-MEIFF',
+    id: 'AMIFE-6AEM-EFIAM-MEIFE4',
     title: 'memo의 제목입니다2',
     description: 'memo의 상세 내용입니다2.',
     createdAt: '2024-06-07',
     updatedAt: '2024-07-02',
+    keyNumber: 4,
   },
   {
-    id: 'AMIFE-6AEM-EFIAM-MEIFG',
+    id: 'AMIFE-6AEM-EFIAM-MEIFE3',
     title: 'memo의 제목입니다3',
     description: 'memo의 상세 내용입니다3.',
     createdAt: '2024-06-08',
     updatedAt: '2024-07-03',
+    keyNumber: 3,
   },
   {
-    id: 'AMIFE-6AEM-EFIAM-MEIFH',
+    id: 'AMIFE-6AEM-EFIAM-MEIFE2',
     title: 'memo의 제목입니다4',
     description: 'memo의 상세 내용입니다4.',
     createdAt: '2024-06-09',
     updatedAt: '2024-07-04',
+    keyNumber: 2,
   },
   {
-    id: 'AMIFE-6AEM-EFIAM-MEIFI',
+    id: 'AMIFE-6AEM-EFIAM-MEIFE1',
     title: 'memo의 제목입니다5',
     description: 'memo의 상세 내용입니다5.',
     createdAt: '2024-06-10',
     updatedAt: '2024-07-05',
+    keyNumber: 1,
   },
   {
-    id: 'AMIFE-6AEM-EFIAM-MEIFJ',
+    id: 'AMIFE-6AEM-EFIAM-MEIFE0',
     title: 'memo의 제목입니다6',
     description: 'memo의 상세 내용입니다6.',
     createdAt: '2024-06-11',
     updatedAt: '2024-07-06',
+    keyNumber: 0,
   },
 ];
