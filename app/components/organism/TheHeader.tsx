9d9bd9ad5ec3b345a1c8ee9247213d0881255483
codemodel6 <codemodel6@gmail.com>
2024-08-23 17:27:09 +0900

    feat : 페이지에 따라 버튼 보여주는 기능 제작 완

--- app/components/organism/TheHeader.tsx
+++ app/components/organism/TheHeader.tsx
@@ -3,7 +3,11 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {globalDisplay} from '../../assets/styles/global/globalDisplay';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../redux/store';
-import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  NavigationProp,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
 import {RootStackParamList} from '../../navigation/type';
 import BackArrowIcon from '../../assets/svgIcon/BackArrowIcon';
 import SearchIcon from '../../assets/svgIcon/SearchIcon';
@@ -13,9 +17,13 @@ const TheHeader = () => {
   const serverMemoListData = useSelector(
     (state: RootState) => state.memoReducer,
   );
-
+  const route = useRoute();
   const memoListDataLength = serverMemoListData.reduxMemoListData.length; // reduxMemoListData 배열의 길이
   const navigation = useNavigation<NavigationProp<RootStackParamList>>(); // 화면 이동 navigation
+
+  // 현재 페이지의 이름을 확인
+  const isMemoListPage = route.name === 'MemoListPage';
+  const isMemoForm = route.name === 'MemoForm';
 
   /** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   - 함수 기능 : 메모리스트에 메모 추가를 위해 메모 추가 페이지로 이동
@@ -25,44 +33,64 @@ const TheHeader = () => {
   };
   return (
     <View style={styles.headerWrapper}>
-      <TouchableOpacity
-        style={styles.headerBackButton}
-        onPress={handleNavigate}>
-        <BackArrowIcon width={24} height={24} />
-      </TouchableOpacity>
+      {isMemoForm && (
+        <TouchableOpacity
+          style={styles.headerBackButton}
+          onPress={handleNavigate}>
+          <BackArrowIcon width={24} height={24} />
+        </TouchableOpacity>
+      )}
       <Text style={styles.headerTitle}>메모리스트({memoListDataLength})</Text>
-      <TouchableOpacity style={styles.headerSearchButton}>
-        <SearchIcon width={24} height={24} />
-      </TouchableOpacity>
+      {isMemoListPage && (
+        <TouchableOpacity style={styles.headerSearchButton}>
+          <View style={styles.svgWrapper}>
+            <SearchIcon width={20} height={20} />
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   headerWrapper: {
-    ...globalDisplay.betweenRow,
+    ...globalDisplay.centerRow,
     height: 70,
     backgroundColor: '#000000',
     paddingHorizontal: 10,
     paddingTop: 15, // 상태바 고려 설정
   },
   headerBackButton: {
-    ...globalDisplay.centerColumn,
-    // backgroundColor: '#ffffff',
+    // ...globalDisplay.centerColumn,
+    // top: 10,
+    position: 'absolute',
+    left: 10,
+    top: '50%',
+    backgroundColor: '#b51717',
     width: 30,
     height: 30,
-    borderRadius: 15,
   },
   headerTitle: {
     fontSize: 20,
     color: '#ffffff',
   },
   headerSearchButton: {
-    ...globalDisplay.centerColumn,
-    // backgroundColor: '#ffffff',
+    // ...globalDisplay.centerColumn,
+    position: 'absolute',
+    right: 10,
+    paddingTop: 15,
+    top: '50%',
+    backgroundColor: '#b51717',
     width: 30,
     height: 30,
-    borderRadius: 15,
+    justifyContent: 'center', // 세로 중앙 정렬
+    alignItems: 'center', // 가로 중앙 정렬
+  },
+  svgWrapper: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
